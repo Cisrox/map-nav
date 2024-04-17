46af77466d659a8a20ad4092ef1f1e630e86cb30
cisrox <graether@ufl.edu>
2024-04-17 18:44:20 -0400

    Adds a test to verify generation of a simple 3x3 map.

--- test/test.cpp
+++ test/test.cpp
@@ -3,7 +3,20 @@
 //
 
 #include <catch2/catch_test_macros.hpp>
+#include "nodemap.hpp"
 
 TEST_CASE("Compile Test", "[flag]"){
     REQUIRE(true);
+}
+
+TEST_CASE("Simple 3x3 grid generation", "[flag]"){
+    nodeMap testMap(3, 3);
+    std::string generatedString = testMap.getMapString();
+    std::string expectedString = R"(XXX
+XXX
+XXX
+)";
+
+
+    REQUIRE(generatedString == expectedString);
 }
