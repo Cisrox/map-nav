06201ecfcea71371c97f38138ae01f84dfb644b6
cisrox <graether@ufl.edu>
2024-04-18 15:40:09 -0400

    Adds documentation/comments to nodemap header file

--- src/nodemap.hpp
+++ src/nodemap.hpp
@@ -22,39 +22,43 @@
 #define SOUTHEAST 6
 #define SOUTHWEST 7
 
+// A grid based data structure that stores nodes
 class nodeMap {
 
     class node {
-        std::pair<int, int> position;
-        sf::CircleShape circle;
-        bool wall = false;
-        std::vector<bool> outEdges;
+        std::pair<int, int> position; // (x, y)
+        sf::CircleShape circle; // Visual representation of the node
+        bool wall = false; // Whether this is a wall (cannot be traversed onto)
+        std::vector<bool> outEdges; // Vector of bools of max size 8 (indexes 0 - 7).
+                                    // Each index corresponds to the same direction for each node.
 
     public:
-        std::string name = "X";
-        bool isWall() {return wall;};
+        std::string name = "X"; // Node's name
+        bool isWall() {return wall;}; // Returns if it is a wall
         node(int x, int y);
-        std::pair<int, int> getPos() {return position;};
-        int getX() {return getPos().first;};
-        int getY() {return getPos().second;};
-        bool isEdge(int& edgePosition) {return outEdges[edgePosition];};
-        bool isCorner(int& xMax, int& yMax);
-        bool isBorder(int& xMax, int& yMax);
-        bool checkBoundary(int& xMax, int& yMax, int& edgePosition);
-        int getOutdegree();
-        void addOutEdge(int& randomPosition);
+        std::pair<int, int> getPos() {return position;}; // Returns position pair
+        int getX() {return getPos().first;}; // Returns x
+        int getY() {return getPos().second;}; // Returns y
+        bool isEdge(int& edgePosition) {return outEdges[edgePosition];}; // Returns if there is an edge in direction
+        bool isCorner(int& xMax, int& yMax); // Returns if this node is in the corner of the board
+        bool isBorder(int& xMax, int& yMax);  // Returns if this node is in the border of the board
+        bool checkBoundary(int& xMax, int& yMax, int& edgePosition); // Returns if an edge would direct out of bounds
+        int getOutdegree(); // Returns the out degree of the node.
+        void addOutEdge(int& randomPosition); // Adds an edge in the given direction.
     };
 
-    void populateMap(int& x, int& y);
-    void generateEdges(int& maxConnections);
-    std::vector<std::vector<std::unique_ptr<node>>> map;
-    int maxX;
-    int maxY;
+    void populateMap(int& x, int& y); // Fills the map[y][x] vector with rudimentary nodes.
+    void generateEdges(int& maxConnections); // Generates desired amount of out edges for each node in map[y][x]
+
+    std::vector<std::vector<std::unique_ptr<node>>> map; // Note: This is organized like [y][x].
+                                                        // unique_ptr cannot be copied, so nodes must be passed by
+                                                        // reference. Loop through like for(auto& x : map) {...}
+    int maxX; // Boundary value for x
+    int maxY; // Boundary value for y
 
 public:
-    std::string getMapString();
-    std::unique_ptr<node>& getNode(int x, int y) { return map[y][x];};
-    void print() {std::cout << this->getMapString();};
-    nodeMap(int x, int y, int maxOutDegree = 8, bool walls = false);
-
+    std::string getMapString(); // Returns a string representation of the nodemap, using node names.
+    std::unique_ptr<node>& getNode(int x, int y) { return map[y][x];}; // Returns the reference to node at [y][x]
+    void print() {std::cout << this->getMapString();}; // Prints string representation of nodemap
+    nodeMap(int x, int y, int maxOutDegree = 8, bool walls = false); // Generates a new nodemap with given parameters.
 };
